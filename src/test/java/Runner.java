fae5cc6b8a33ca15a3d8135dd1ebacee8cccfff7
eshemiakin <evgeniy.shemiakin@cengage.com>
2019-06-21 17:02:20 +0300

    Fix runner class to use testng annotations instead of junit

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -1,9 +1,6 @@
-import org.junit.runner.RunWith;
-
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features")
-public class Runner {
+@CucumberOptions(features = "src/test/resources/features", glue = "stepdefs")
+public class Runner extends AbstractTestNGCucumberTests {
 }
